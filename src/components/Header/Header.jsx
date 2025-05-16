03b72234c7bed3a4f860def12aaf906366f10a07
Micael Leite <micaelleite01@gmail.com>
2025-05-16 16:55:19 -0300

    feat: add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,45 @@
 import React from "react"
+import "./Header.css"
 
 const Header = () => {
   return (
-    <header class="fixed ">
-      <div>
-        <img src="../" alt="" />
+    <header className="fixed top-2 left-1/2 -translate-x-1/2 w-full max-w-7xl mx-auto z-50 h-16 flex justify-between items-center px-5 py-2">
+      <img src="/LOGO_MKL.svg" alt="logo" className="w-20" />
+      <div className="p-0.5 bg-gradient-to-r from-[#C04406] to-[#CE89F8] rounded-full nav-bars">
+        <div className="bg-black backdrop-blur-xl rounded-full">
+          <nav className="px-5 py-1.5">
+            <ul className="flex space-x-9">
+              <li>
+                <a href="#home" className="text-white hover:text-gray-300">
+                  Início
+                </a>
+              </li>
+              <li>
+                <a href="#about" className="text-white hover:text-gray-300">
+                  Sobre
+                </a>
+              </li>
+              <li>
+                <a href="#services" className="text-white hover:text-gray-300">
+                  Projetos
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <nav></nav>
+      <div className="flex items-center space-x-4">
+        <input
+          type="button"
+          value="Contato"
+          className="bg-gradient-to-r from-[#C04406] to-[#CE89F8] text-white font-medium px-5 py-0.5 rounded-full hover:"
+        />
+        <img
+          src="/menu.svg"
+          alt=""
+          className="menu-hamburguer hidden cursor-pointer"
+        />
+      </div>
     </header>
   )
 }
